969217cde9f80fe4eaf37a2a4fc276583b7b3b85
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-03-24 11:50:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ sudo pacman -S - < requirements.txt
 # Copy config files
 cp -r "config/openbox/" ~/.config/
 cp "config/picom/picom.conf" ~/.config/
+
+# Enable ligthdm
+sudo systemctl enable lightdm
