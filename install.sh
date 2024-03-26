34e6fe0c0f8194c32de39e5d76f8f8adfe7cbc8e
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-03-26 05:28:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir ~/.config/
 # Copy config files
 cp -r "config/openbox" ~/.config/
 cp -r "config/jgmenu" ~/.config/
+cp -r "config/polybar" ~/.config/
+cp -r "config/Thunar" ~/.config/
+cp "config/picom.conf" ~/.config/
 
 # Enable ligthdm
 sudo systemctl enable lightdm
