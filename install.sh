abc8933c9095d09672acfe98eba3ae13adb4ea1d
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-05-04 14:52:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+command() {
+  # Instalation dependencies
+  sudo pacman -Sy
+  sudo pacman -S - < requirements.txt
 
-# Install required dependencies
-sudo pacman -S - < requirements.txt
+  sudo systemctl enable lightdm
+  xdg-user-dirs-gtk-update
+  
+  # Configurations
+  mkdir ~/.config
+  mkdir ~/.fonts
+  
+  # Copy configurations for Desktop environment
+  cp -r config/* ~/.config
+  
+  cd ~/.fonts
+  wget https://github.com/ryanoasis/powerline-extra-symbols/raw/master/PowerlineExtraSymbols.otf
+}
 
-mkdir ~/.config/
-
-# Copy config files
-cp -r "config/" ~/.config/
-
-# Enable ligthdm
-sudo systemctl enable lightdm
+command
