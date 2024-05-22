ee8f21757ebe884f4d7afde60a473e10359690bf
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-05-21 20:19:28 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo pacman -Rsnc - < requirements.txt
 
-rm -r ~/.config/openbox/
-rm ~/.config/picom.conf
+rm -r ~/.config/Thunar
+rm -r ~/.config/openbox
+rm -r ~/.config/picom
+rm -r ~/.config/polybar
+rm -r ~/.config/pwoff
+rm -r ~/.config/sxhkd
