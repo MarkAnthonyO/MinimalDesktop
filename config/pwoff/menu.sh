e84430f96d1ee36c690e0fe07e915db1f6e63352
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-05-21 20:16:26 -0600

    Create menu.sh

---
+++ config/pwoff/menu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+yad --image="info" --text="The PC will shutting down, are you sure?" --button="Shutting down":0 --button="Cancel":1
+
+ans=$?
+
+if [ $ans -eq 0 ]
+then
+	echo "Shutting down"
+	shutdown now
+
+else
+	echo "Canceling"
+fi
