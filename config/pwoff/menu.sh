fbed71df9aad4a4486d0e96b524a550f339bbb33
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-05-23 23:17:20 -0600

    Update menu.sh

--- config/pwoff/menu.sh
+++ config/pwoff/menu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yad --image="info" --text="The PC will shutting down, are you sure?" --button="Shutting down":0 --button="Cancel":1
+yad --center --image="info" --text="The PC will shutting down, are you sure?" --button="Shutting down":0 --button="Cancel":1
 
 ans=$?
 
