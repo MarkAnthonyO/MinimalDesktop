7deb5bc68a11d8ecaba7d61c0d79ee4b22120903
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-06-05 21:23:15 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,6 @@ rm -r ~/.config/openbox
 rm -r ~/.config/picom
 rm -r ~/.config/polybar
 rm -r ~/.config/pwoff
-rm -r ~/.config/sxhkd
 rm ~/.fonts/PowerlineExtraSymbols.otf
 
 sudo rm -r ~/.themes/MinD-theme
