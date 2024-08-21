8f63eeb34fe829ec23be5b463a8d21fa95277154
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-08-21 14:01:40 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,14 @@
 sudo pacman -Rsnc - < requirements.txt
 
 rm -r ~/.config/Thunar
+rm -r ~/.config/dunst
+rm -r ~/.config/gtk-3.0
 rm -r ~/.config/openbox
 rm -r ~/.config/picom
+rm -r ~/.config/plank/dock1
 rm -r ~/.config/polybar
 rm -r ~/.config/pwoff
+rm -r ~/.icons/default/index.theme
 
-rm -rf ~/.themes/MinD-theme
+sudo rm -rf /usr/share/applications/AppLauncher.desktop
+sudo rm -r /etc/lightdm/
